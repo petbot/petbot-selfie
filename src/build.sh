5dddab2ea864b50b1948662ec83d0585a4ba52c9
Misko <misko@cs.toronto.edu>
2015-07-25 22:37:16 +0000

    updated build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
-gcc -o atos atos.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3 -lrt
-gcc -o train train.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3
-gcc -o load load.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3
+gcc -o atos atos.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3 -lrt -fopenmp
+gcc -o train train.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3 -fopenmp
+gcc -o load load.c  -L/usr/lib -ljpcnn -lpthread -Wall -O3 -fopenmp
 cd v4l2grab
 sh build.sh
 cd ..
